0ad675c8eac4c17971ae0411bc475d3d26c943d2
Levi Liester <levi.liester@gmail.com>
2023-01-11 21:57:19 -0600

    DO IT

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port = process.env.PORT || 3000;
+app.use(cors());
+const baseUrl = process.env.BASE_URL ||`http://localhost:${port}`
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const urlMaps = {}
+
+const generateUrlCode = () => {
+  const length = 6
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+app.get('/:urlCode', (req, res) => {
+  const {urlCode} = req.params;
+  if(!urlCode){
+    res.status(409)
+    return;
+  }
+  const redirectUrl = urlMaps[urlCode];
+  res.redirect(302, redirectUrl);
+})
+
+app.get('/shorten/url', (req, res) => {
+  const {url} = req.query;
+  if(!url){
+    res.status(409)
+    return
+  }
+  const urlCode = generateUrlCode()
+  urlMaps[urlCode] = url
+  res.send(`${baseUrl}/${urlCode}`)
 })
 
 app.listen(port, () => {
